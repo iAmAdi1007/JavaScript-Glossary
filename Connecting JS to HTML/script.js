4a9e3e00de2bdbe217e1807e9e1f36a21a3d153a
iAmAdi1007 <pratapaditya516@gmail.com>
2022-05-29 01:53:15 +0530

    Added Connecting JS to HTML

---
+++ Connecting JS to HTML/script.js
@@ -0,0 +1,15 @@
+// window.addEventListener("DOMContentLoaded", main);
+
+// function main(){
+//     document.querySelector('button').addEventListener("click",changeBackgroundColor);
+    
+//     function changeBackgroundColor(){
+//         window.document.body.style.backgroundColor = "#00334C"
+//     }
+// }
+
+document.querySelector('button').addEventListener("click",changeBackgroundColor);
+
+function changeBackgroundColor(){
+    window.document.body.style.backgroundColor = "#00334C"
+};
